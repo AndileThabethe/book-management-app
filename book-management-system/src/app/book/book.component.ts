ab58dcbbd4629cebb40bcf279ff1b4c713166270
AndileThabethe <151631281+AndileThabethe@users.noreply.github.com>
2025-01-09 14:30:06 +0200

    feat: change list to grid

--- book-management-system/src/app/book/book.component.ts
+++ book-management-system/src/app/book/book.component.ts
@@ -13,6 +13,7 @@ export class BookComponent implements OnInit{
   newBookAuthor : string = "";
 
   books: Book[] = []
+  editIndex: number | null = null; 
 
   ngOnInit(): void {
     let savedBooks = localStorage.getItem("books")
@@ -38,4 +39,23 @@ export class BookComponent implements OnInit{
   deleteBook(index: number){
     this.books.splice(index, 1)
   }
+
+  editBook(index: number) {
+    this.editIndex = index; 
+    this.newBookTitle = this.books[index].title; 
+    this.newBookAuthor = this.books[index].author; 
+    this.books.splice(index, 1)
+  }
+
+  updateBook() {
+    if (this.editIndex !== null) {
+      this.books[this.editIndex].title = this.newBookTitle;
+      this.books[this.editIndex].author = this.newBookAuthor;
+      this.editIndex = null; 
+      this.newBookTitle = "";
+      this.newBookAuthor = "";
+      localStorage.setItem("books", JSON.stringify(this.books)); 
+    }
+  }
+
 }
