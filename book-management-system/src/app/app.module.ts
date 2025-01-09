1ccde363f338ac76ed0aa485f1c0afb636f7c66c
AndileThabethe <151631281+AndileThabethe@users.noreply.github.com>
2025-01-09 11:04:22 +0200

    feat: style with css

--- book-management-system/src/app/app.module.ts
+++ book-management-system/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { BookComponent } from './book/book.component';
     BookComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
